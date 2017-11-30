a8500c86183b974031347094e31c10d7b9edd319
_lavalamp <cegrayson3@gmail.com>
2017-11-29 16:15:23 -0800

    Updated scripts to reflect new Docker naming convention.

--- scripts/run-tasknode-deploy-checker.sh
+++ scripts/run-tasknode-deploy-checker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker-compose up -d
-TASKNODE="$(docker ps | grep wsbackend-tasknode | awk '{print $1}')"
+TASKNODE="$(docker ps | grep ws | grep tasknode | awk '{print $1}')"
 docker exec -it $TASKNODE scripts/check_deployment.sh
 docker-compose stop
 docker-compose rm
